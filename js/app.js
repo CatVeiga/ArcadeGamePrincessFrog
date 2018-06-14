41e8337199b58a407a4cbf69c6ddd94ca90227fc
cveiga <cveiga819@gmail.com>
2018-06-14 20:13:02 +0100

    fix the bugs

--- js/app.js
+++ js/app.js
@@ -1,12 +1,8 @@
-//TODO: create a timer function (my timer does not work)
 //TODO: create a grab function for it's easier to reach the points
 //TODO: create a different sound for different stage the game (hit, die, win, grab...)
 
 //──── global variables ──────────────────────────────────────────────────────────────────
-const liveContainer = document.querySelector(".lives");
-let lives = 3;
-const scoreContainer = document.querySelector(".score");
-let score = 0;
+//variables for audio
 const play = document.getElementById("audio");
 let oldsrc = play.src;
 
@@ -15,23 +11,127 @@ let oldsrc = play.src;
  *                              modal for the instructions                              *
  *                                                                                      */
 //========================================================================================
-const modalInst = document.getElementById("modalinst");
-const btnInst = document.getElementById("btn-inst");
-const close = document.getElementById("close");
+const close = document.querySelector("#close");
+const modalInst = document.querySelector("#modalinst");
+const btnInst = document.querySelector("#btn-inst");
 
-btnInst.onclick = function() {
+
+close.addEventListener("click", function(){
+    modalInst.style.display = "none"; 
+    startTime(); 
+    oldsrc = play.src;  
+});
+
+btnInst.addEventListener("click", function(){
     modalInst.style.display = "block";
-}
+    play.src = "";
+});
 
-close.onclick = function() {
-    modalInst.style.display = "none";
-}
-//when user clicks anywhere outside the modal it closes
-window.onclick = function() {
-    if(event.target == modalInst) {
-        modalInst.style.display = "none";
+//========================================================================================
+/*                                                                                      *
+ *                              function to init the timer                              *
+ *                                                                                      */
+//========================================================================================
+const timeContainer = document.querySelector("#timer");
+const modalOver = document.querySelector("#modalOver");
+const modalWin = document.querySelector("#modalWin");
+let seconds = 0;
+let totalTime = 120;
+
+function startTime(){
+    timeContainer.innerHTML = totalTime - seconds + `(s)`;
+    const interval = setInterval(initTime, 1000);
+
+    function initTime() {
+        seconds++;
+        timeContainer.innerHTML = totalTime - seconds + `(s)`;
+        //if the seconds was equal to totalTime
+        if(seconds == totalTime) {
+            //player will reset the position
+            player.x = 202;
+            player.y = 405;
+            //shows the modal
+            modalOver.style.display = "block";
+            //clears the timer
+            clearInterval(interval);
+            seconds = 0;
+            firstMove = false;
+            //the audio will stop
+            play.src = "";
+        }
+
+        //if the user reaches 1600 points 
+        if(score == 1600) {
+            // resets the player position
+            player.x = 202;
+            player.y = 405;
+            //shows the modal
+            modalWin.style.display = "block";
+            //clears the timer
+            clearInterval(interval);
+            seconds = 0;
+            firstMove = false;
+            // the audio will stop
+            play.src = "";
+        }
+
+        // if the user loses all the lives
+        if (lives == 0) {
+            //resets the player position
+            player.x = 202;
+            player.y = 405;
+            //show thwe modal
+            modalOver.style.display = "block";
+            clearInterval(interval);
+            seconds = 0;
+            firstMove = false;
+            //the audio will stop
+            play.src = "";
+        }
     }
 }
+
+//========================================================================================
+/*                                                                                      *
+ *                               function to init the game                              *
+ *                                                                                      */
+//========================================================================================
+const liveContainer = document.querySelector(".lives");
+let lives;
+const scoreContainer = document.querySelector(".score");
+let score;
+let firstMove;
+
+function initGame() {
+    lives = 3;
+    score = 0;
+    firstMove = true;
+    liveContainer.innerHTML = lives;
+    scoreContainer.innerHTML = score;
+    timeContainer.innerHTML = totalTime + `(s)`;
+};
+
+initGame();
+
+//========================================================================================
+/*                                                                                      *
+ *                                   events listeners                                   *
+ *                                                                                      */
+//========================================================================================
+const btnOver = document.querySelector("#btn-over");
+const btnWin = document.querySelector("#btn-win");
+
+btnOver.addEventListener("click", function(){
+    modalOver.style.display = "none";
+    initGame();
+    startTime();
+});
+
+btnWin.addEventListener("click", function(){
+    modalWin.style.display = "none";
+    initGame();
+    startTime();
+});
 
 //========================================================================================
 /*                                                                                      *
@@ -39,123 +139,46 @@ window.onclick = function() {
  *                                                                                      */
 //========================================================================================
 //──── the enemies we must avoid ─────────────────────────────────────────────────────────
-var Enemy = function(x, y, speed) {
-    //defining the exact location where enemies should be and the speed
-    this.x = x;
-    this.y = y;
-    this.speed = speed;
-    //the image we will use to set our enemies
-    this.sprite = 'images/enemy-car.png';
-};
-
-//──── update the position with parameter: dt ────────────────────────────────────────────
-Enemy.prototype.update = function(dt) {
-    //we will multiply the speed
-    this.x += this.speed * dt;
-    //we will ensure that the game runs at the same time for all computers
-    if(this.x > 510) {
-        this.x = -50;
-        this.speed = 100 + Math.floor(Math.random() * 200);
+class Enemy {
+    constructor(x, y, speed) {
+        //defining the exact location where enemies should be and the speed
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+        //the image we will use to set our enemies
+        this.sprite = 'images/enemy-car.png';
     }
-    //when the player hit on the enemy it should return to the start position
-    if(player.x < this.x + 80 && 
-		player.x + 80 > this.x && 
-		player.y < this.y + 60 && 
-		60 + player.y > this.y) {
-            //reset the position
-            player.reset();
-            //decrease lives
-            lives--;
-            if(lives == 0) {
-                //shows the over modal
-                loseGame();
-                //game restart
-                restart();
-                //clear the timer
-                //clearInterval(interval);
-
-            }
+    //──── update the position with parameter: dt ────────────────────────────────────────────
+    update(dt) {
+        this.render();
+        //we will multiply the speed
+        this.x += this.speed * dt;
+        //we will ensure that the game runs at the same time for all computers
+        if (this.x > 510) {
+            this.x = -50;
+            this.speed = 100 + Math.floor(Math.random() * 200);
         }
-};
-
-//──── draw the Enemy on the screen ──────────────────────────────────────────────────────
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-//========================================================================================
-/*                                                                                      *
- *                             function to restart the game                             *
- *                                                                                      */
-//========================================================================================
-function restart() {
-    //reset the player position
-    player.reset();
-    //reset the lives 
-    lives = 3;
-    // reset the score 
-    score = 0;
-    //clear the time
-    //clearInterval(interval);
-}
-
-//========================================================================================
-/*                                                                                      *
- *                                   function to timer                                  *
- *                                                                                      */
-//========================================================================================
-/*let timer = document.querySelector("#timer");
-*let seconds = 60;
-*
-function startTime() {
-*    
-*    const interval = setTimeout(function(){
-*        timer.innerHTML = seconds;
-*        seconds--;
-*        if(seconds = 0){
-*           loseGame();
-*            seconds = 60;
-*        }
-*    }, 1000);
-*}
-*/
-//========================================================================================
-/*                                                                                      *
- *                              modal box for over the game                             *
- *                                                                                      */
-//========================================================================================
-const modalOver = document.querySelector("#modalOver");
-const btnOver = document.querySelector("#btn-over");
-
-function loseGame() {
-    if(lives === 0){
-        modalOver.style.display = "block";
-        play.src = "";
-        restart();
+      //when the player colide with the enemys check for collisions
+      checkCollisions();
     }
-    btnOver.onclick = function() {
-        modalOver.style.display = "none";
-        
+    //──── draw the Enemy on the screen ──────────────────────────────────────────────────────
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 }
 
-//========================================================================================
-/*                                                                                      *
- *                              modal box for win the game                              *
- *                                                                                      */
-//========================================================================================
-const modalWin = document.querySelector("#modalWin");
-const btnWin = document.querySelector("#btn-win");
-
-function winGame() {
-    if(scores === 1600) {
-        modalWin.style.display = "block";
-        play.src = "";
-    }
-    btnWin.onclick = function() {
-        modalWin.style.display = "none";
-        restart();
-    }
+function checkCollisions () {
+    allEnemies.forEach(function(enemy){
+        if((player.x < enemy.x + 80 &&
+            player.x + 55 > enemy.x) &&
+            (player.y < enemy.y + 60 &&
+            player.y + 60 > enemy.y)) {
+                player.x = 202;
+                player.y = 405;
+                lives--;
+                liveContainer.innerHTML = lives;
+            }
+    });
 }
 
 //========================================================================================
@@ -164,56 +187,48 @@ function winGame() {
  *                                                                                      */
 //========================================================================================
 //──── setting the player, location ──────────────────────────────────────────────────────
-var Player = function(x, y) {
-    //defining the location where the player will start
-    this.x = x;
-    this.y = y;
-    //the image will we use to set the player
-    this.player = 'images/Design.png';    
-}
-
-//──── update the player Position ────────────────────────────────────────────────────────
-Player.prototype.update = function(dt) {
-    //show the lives 
-    liveContainer.innerHTML = `${lives}`;
-    //shows the score in the game
-    scoreContainer.innerHTML = `${score}`;
-}
-
-//──── draw the player in the screen ─────────────────────────────────────────────────────
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.player), this.x, this.y);
-}
-
-//──── reseting the player to the start position ─────────────────────────────────────────
-Player.prototype.reset = function() {
-    this.x = 202;
-    this.y = 405;
-}
-
-//──── using the handleinput to keypress ─────────────────────────────────────────────────
-Player.prototype.handleInput = function(keyPress) {
-    if(keyPress == 'up' && this.y > 0) {
-        this.y -= 83;
+class Player {
+    constructor(x, y) {
+        //defining the location where the player will start
+        this.x = x;
+        this.y = y;
+        //the image will we use to set the player
+        this.sprite = 'images/Design.png';
     }
-    if(keyPress == 'down' && this.y < 405) {
-        this.y += 83;
+    //──── update the player Position ────────────────────────────────────────────────────────
+    update(dt) {
+        this.render();
     }
-    if(keyPress == 'left' && this.x > 0) {
-		this.x -= 102;
+    //──── draw the player in the screen ─────────────────────────────────────────────────────
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
-    if (keyPress == 'right' && this.x < 405) {
-		this.x += 102;
-    }
-    if (this.y < 0) {
-		setTimeout(function() {
-            //when the player reaches the water he will reset to start position
-			player.x = 202;
+    //──── using the handleinput to keypress ─────────────────────────────────────────────────
+    handleInput(keyPress) {
+        if (keyPress == 'up' && this.y > 0) {
+            this.y -= 83;
+        }
+        if (keyPress == 'down' && this.y < 405) {
+            this.y += 83;
+        }
+        if (keyPress == 'left' && this.x > 0) {
+            this.x -= 102;
+        }
+        if (keyPress == 'right' && this.x < 405) {
+            this.x += 102;
+        }
+        if (this.y < 0) {
+            setTimeout(function(){
+            //reset the player position
+            player.x = 202;
             player.y = 405;
-            //increase at the same time the score to 100 points
-			score += 100;
-		}, 100);
-	}
+            //increase the score 
+            score += 100;
+            scoreContainer.innerHTML = score;
+
+            }, 100);
+        }
+    }
 }
 
 //========================================================================================
@@ -244,6 +259,7 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     };
-
-    player.handleInput(allowedKeys[e.keyCode]);
+    if(firstMove){
+        player.handleInput(allowedKeys[e.keyCode]);
+    }    
 });
